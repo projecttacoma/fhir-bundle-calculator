ec1c727d16036262b9d68c8a2b671bed67d55dcc
Michael O'Keefe <mokeefe@mitre.org>
2020-08-27 13:58:30 -0400

    Remove async tag from methods that don't need to be async

--- test/fhirpath.test.js
+++ test/fhirpath.test.js
@@ -1,14 +1,14 @@
 const { getPopulationCount } = require('../src/utils/fhirpath');
 const ipopMeasureReport = require('./fixtures/ipop-measure-report.json');
 
-test('Nonzero population that exists should return the right count', async () => {
+test('Nonzero population that exists should return the right count', () => {
   expect(getPopulationCount(ipopMeasureReport, 'initial-population')).toEqual(1);
 });
 
-test('Zero population that exists should return the right count', async () => {
+test('Zero population that exists should return the right count', () => {
   expect(getPopulationCount(ipopMeasureReport, 'numerator')).toEqual(0);
 });
 
-test('Nonexistent population should return null', async () => {
+test('Nonexistent population should return null', () => {
   expect(getPopulationCount(ipopMeasureReport, 'measure-observation')).toEqual(null);
 });
