6299640f157b8d906848b44c7c9327cb10ffd1e5
Michael O'Keefe <mokeefe@mitre.org>
2020-08-27 13:48:51 -0400

    Add better support for populations that don't exist (return null, rather than erroring out)

--- src/utils/fhirpath.js
+++ src/utils/fhirpath.js
@@ -2,5 +2,10 @@ const fhirpath = require('fhirpath');
 
 exports.getPopulationCount = (mr, population) => {
   const path = fhirpath.compile(`MeasureReport.group.population.where(code.coding.code = '${population}')`);
-  return path(mr)[0].count;
+  const result = path(mr);
+
+  if (!!result && result[0] !== undefined) {
+    return result[0].count;
+  }
+  return null;
 };
